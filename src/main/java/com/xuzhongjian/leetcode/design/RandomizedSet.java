48ac7cef872c552f33893bf1830f946f2ba1c5e8
xuzhongjian <xuzhongjian@itiger.com>
2024-11-03 22:02:39 +0800

    feat: add lc.274

--- src/main/java/com/xuzhongjian/leetcode/design/RandomizedSet.java
+++ src/main/java/com/xuzhongjian/leetcode/design/RandomizedSet.java
@@ -1,5 +1,11 @@
 package com.xuzhongjian.leetcode.design;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+
+
 /**
  * 274
  * Your RandomizedSet object will be instantiated and called as such:
@@ -10,19 +16,26 @@ package com.xuzhongjian.leetcode.design;
  */
 public class RandomizedSet {
 
-    public RandomizedSet() {
+    private final HashSet<Integer> set;
+    private final Random random;
 
+    public RandomizedSet() {
+        set = new HashSet<>();
+        random = new Random();
     }
 
-//    public boolean insert(int val) {
-//
-//    }
-//
-//    public boolean remove(int val) {
-//
-//    }
-//
-//    public int getRandom() {
-//
-//    }
+    public boolean insert(int val) {
+        return set.add(val);
+    }
+
+    public boolean remove(int val) {
+        return set.remove(val);
+    }
+
+    public int getRandom() {
+        int size = set.size();
+        int i = random.nextInt(size);
+        List<Integer> list = new ArrayList<>(set);
+        return list.get(i);
+    }
 }
