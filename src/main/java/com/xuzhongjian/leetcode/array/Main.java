ecec6e7ddcca50aba7db46ebf6e1b3e31d792cdd
Ganten Xu <zjxu97@gmail.com>
2024-11-07 00:37:45 +0800

    feat: daily leetcode

--- src/main/java/com/xuzhongjian/leetcode/array/Main.java
+++ src/main/java/com/xuzhongjian/leetcode/array/Main.java
@@ -1,11 +1,9 @@
 package com.xuzhongjian.leetcode.array;
 
-import com.xuzhongjian.leetcode.string.Compress;
-
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5};
-        MinSubArrayLen main = new MinSubArrayLen();
-        System.out.println(main.minSubArrayLen(11, nums));
+        int[] nums = { 8, 4, 2, 30, 15 };
+        CanSortArray main = new CanSortArray();
+        System.out.println(main.canSortArray(nums));
     }
 }
